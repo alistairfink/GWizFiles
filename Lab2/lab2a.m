96c82d2b246847fc7dc064accc003171d9cb9536
Alistair Ying Che Fink <aycfink@uwaterloo.ca>
2019-10-26 22:31:29 -0400

    fuck my fucking life fuck this

---
+++ Lab2/lab2a.m
@@ -0,0 +1,28 @@
+format long
+
+s = tf('s');
+t = 0.040948;
+p = 1.88/(0.068*s^2+s);
+timeDelay = (1-(t/4)*s)/(1+(t/4)*s);
+%P = p*timeDelay;
+P = p;
+
+%Lambda = [-10, -190, -190, -190, -190];
+Lambda = [-8+3*1i, -8-3*1i, -50];
+%Lambda = [-9+8i, -9-8i, -20];
+C = pp(P,Lambda)
+stepinfo(feedback(P*C, 1))
+bw = bandwidth(feedback(P*C, 1))
+T = 2*pi/(25*bw);
+discreteSystem = c2d(C, T, 'tustin')
+
+
+[y,t] = step(minreal(feedback(P*C,1)));
+
+figure('Position', [300, 100, 800, 395]);
+plot(t, y, 'b', 'linewidth', 2);
+grid on;
+set(gca, 'FontSize', 16);
+xlabel('Time (s)','interpreter','latex','FontSize',20);
+ylabel('Angular position (radians)','interpreter','latex','FontSize',20);
+xlim([0,max(t)])
