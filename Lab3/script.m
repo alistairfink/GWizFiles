e500363a0b7a20fbea81bb5a33e392ad38303010
Alistair Fink <alistairfink@gmail.com>
2019-11-10 07:47:33 -0500

    kinda working

--- Lab3/script.m
+++ Lab3/script.m
@@ -17,13 +17,29 @@ k2 = 0.061;
 k3 = 4.78/(s^2);
 p2 = k2*k3/s;
 
-real1 = -0.6;
-img1 = 0.01;
+real1 = -0.61;
+img1 = 0.1;
 
-real2 = -2;
-img2 = 2;
+real2 = -1.91;
+img2 = 0.1;
 
-pole3 = -20;
+pole3 = -15;
+
+% real1 = -0.7;
+% img1 = 0.3;
+% 
+% real2 = -1.4;
+% img2 = 0.3;
+% 
+% pole3 = -35;
+
+% real1 = -0.8;
+% img1 = 0.01;
+% 
+% real2 = -1.8;
+% img2 = 1.15;
+% 
+% pole3 = -20;
 
 % real1 = -0.01;
 % img1 = 0.01;
@@ -34,8 +50,14 @@ pole3 = -20;
 % pole3 = -7.5;
 Lambda2 = [real1+img1*1i, real1-img1*1i, real2+img2*1i, real2-img2*1i,  pole3];
 C2 = pp(p2,Lambda2)
-stepinfo(feedback(p2*C2*inner, 1))
-bw = bandwidth(feedback(p2*C2*inner, 1))
+
+% kp = 2.3654;
+% ki = 0.24151;
+% kd = 5.7916;
+% C2 = kp+kd*s+ki/s;
+stepinfo(feedback(p2*C2, 1))
+bw = bandwidth(feedback(p2*C2, 1))
+% T = 2*pi/(25*bw);
 T = 0.029012;
 
 % discreteController = c2d(C2/s, T, 'tustin')
